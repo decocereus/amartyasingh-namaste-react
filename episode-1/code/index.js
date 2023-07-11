f7b26e6208e229ed6f34903b702777f3aa1fd072
Amartya Singh <amartyasingh84@gmail.com>
2023-07-11 20:26:39 +0530

    notes

--- episode-1/code/index.js
+++ episode-1/code/index.js
@@ -1,25 +1,13 @@
-/**
- * * React.createElement returns an *object*
- * * the render function changes that object into the specified type of tag which browser understands
- * * createElement takes in three arguments, type, attributes and children. These three together make the props.
- */
 const headingReact = React.createElement(
   "h1",
   { id: "reactHeading" },
   "Hello World from React"
 );
-/**
- * * created a new root div for react-dom because it was replacing the contents of the existing root.
- * * To see all three methods a new div element was required.
- */
 
 // This is how you create nested elements in react
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  /**
-   * * If we want to pass in multiple children then we need to pass the children inside a list [child1, child2....]
-   */
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am h1"),
     React.createElement("h2", {}, "I am h2"),
